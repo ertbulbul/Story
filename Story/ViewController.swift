52642a1439786d2dfd3ee01ae6c80495c5621d58
Onur Can Avci <onurcan.avci@trendyol.com>
2020-10-17 18:47:19 +0300

    Initial Commit

---
+++ Story/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Story
+//
+//  Created by Onur Can Avcı on 17.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
